9638be9468884c39019e549fa22353ea1e794e53
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-12 20:16:54 +0300

    view single project

--- restF/urls.py
+++ restF/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
   path('',views.home,name='home'),
-  path('search/',views.search_results, name='search_results')
+  path('search/',views.search_results, name='search_results'),
+  re_path('project/(\d+)',views.project, name='project')
 ]
 
 if settings.DEBUG:
--- restF/views.py
+++ restF/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Project,Profile
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 
 # Create your views here.
 def home(request):
@@ -17,3 +19,11 @@ def search_results(request):
   else:
     message="You haven't searched for any item"
     return render(request,'search.html',{'messages':message})
+
+def project(request,project_id):
+  try:
+    project = Project.objects.get(id=project_id)
+  except ObjectDoesNotExist:
+    raise Http404()
+  return render(request,'project.html',{'project':project})
+  redirect('/')
