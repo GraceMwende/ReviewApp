b0aba336007010fc7d7d981483d5e9d907962f80
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-13 16:01:58 +0300

    add auth dropdowns and redirect

--- reviews/urls.py
+++ reviews/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('restF.urls')),
     path('tinymce',include('tinymce.urls')),
-    path('accounts/',include('registration.backends.simple.urls'))
+    path('accounts/',include('registration.backends.simple.urls')),
+    path('logout/',views.LogoutView.as_view(),{'next_page':'/'})
 ]
