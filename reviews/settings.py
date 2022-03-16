3462ab17ef8a565ba2b363c9a7a4d0d862b3c9a8
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-16 22:42:38 +0300

    heroku

--- reviews/settings.py
+++ reviews/settings.py
@@ -182,6 +182,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 # LOGOUT_REDIRECT_URL = 'accounts/login'
 # Default primary key field type
+# Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
